a893ce3a0e5c77dc71b3153b536d95c012f3c992
LorenzoPizzuto <lorenzopizzuto05@gmail.com>
2024-11-04 15:18:55 +0100

    Initial Commit

---
+++ Git Test/Git_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_TestApp.swift
+//  Git Test
+//
+//  Created by Lorenzo Pizzuto on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Git_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
